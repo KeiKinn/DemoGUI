b1948e8de80196c426f386b68ec3375f88aab3f6
JadaGates <jx589g@gmail.com>
2018-07-15 15:20:54 +0800

    mx子程序

--- SourceCode/GUImain.m
+++ SourceCode/GUImain.m
@@ -14,7 +14,7 @@ try
     while(~feof(fl))
         DataTemp = fread(fl, [Channel, DataClipLeng], app.DataFormat)';
         DataTemp = DataTemp(:, 1);
-        TimeTag = (1 : 2500) + counter * 2500;
+        TimeTag = (1 : DataClipLeng) + counter * DataClipLeng;
         plot(app.TimePlot, TimeTag, DataTemp(: ,1));
         drawnow limitrate;
         
@@ -29,16 +29,22 @@ try
         [FFTMax , FFTMaxloc] = max(abs(FFTClipData(10 : end)));
         plot(app.FrePlot, f1 , abs(FFTClipData));
         drawnow limitrate;
+        counter = counter + 1;
+        %%%|| - - -  User Functions  - - - ||%%%
+        SigModFlag = 3;
+        
+        
+        %%- - - Estimate Signal Carrier Frequency & Symbol Rate - - -%%
+        [aSigVal1, aSigVal2, aSigVal3, aSigVal4, aSymVal] = Estimate(DataTemp, fs, SigModFlag);
+        FreField;
+        %%%|| - - -  Stop Flag  - - - ||%%%
         if(app.breakFlag)
             app.breakFlag = 0;
             break;
         end
-        counter = counter + 1;
-        %%%|| - - -  User Functions  - - - ||%%%
-        
     end
 catch
     if fl == -1
-    NoFileError;
+        NoFileError;
     end
 end
